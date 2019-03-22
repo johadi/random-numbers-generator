d0d6c69763f2e5bbc3a57b014e092ca3e5197f8e
johadi <jim.hadi10@gmail.com>
2019-03-23 00:24:38 +0100

    Add a share module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { GenerateNumbersFormComponent } from './generate-numbers-form/generate-numbers-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NumbersDisplaySectionComponent } from './numbers-display-section/numbers-display-section.component';
+import {SharedModule} from './shared/shared.module';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GenerateNumbersFormComponent,
+    NumbersDisplaySectionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
